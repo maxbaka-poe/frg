f7360d48a5cf6c8fa03c4aeae5e7411d0532765f
maxbaka-poe <114225554+maxbaka-poe@users.noreply.github.com>
2026-08-05 03:43:36 +0900

    Update generate_rss.py

--- generate_rss.py
+++ generate_rss.py
@@ -1,8 +1,8 @@
+import sys
 import requests
 from feedgen.feed import FeedGenerator
 import datetime
 import re
-import sys
 
 # ＝＝＝＝＝＝ 設定 ＝＝＝＝＝＝
 # ① RSSを取得したいFANBOXのクリエイターID
@@ -12,12 +12,11 @@ CREATOR_ID = "12372838"
 CREATOR_NAME = "ilu"
 
 # ③ 生成するXMLファイルの名前（バレないようにランダムな文字列にするのがおすすめ）
-# 例: "feed_7a8b9c.xml"
 OUTPUT_FILENAME = "secret_feed_12345.xml"
 # ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
 
 def get_fanbox_posts(creator_id):
-    api_url = f"https://api.fanbox.cc/post.listCreator?creatorId={12372838}&limit=15"
+    api_url = f"https://api.fanbox.cc/post.listCreator?creatorId={creator_id}&limit=15"
     headers = {'Origin': 'https://www.fanbox.cc'}
     try:
         res = requests.get(api_url, headers=headers)
@@ -50,24 +49,38 @@ def main():
         fe = fg.add_entry()
         fe.id(post_url)
         fe.link(href=post_url, rel='alternate')
-        fe.title(post.get('title', '無題'))
         
+        # タイトルの取得
+        title = post.get('title', '無題')
+        fe.title(title)
+        
+        # 日時の取得
         pub_date = post.get('publishedDatetime')
         if pub_date:
             dt = datetime.datetime.fromisoformat(pub_date.replace('Z', '+00:00'))
             fe.published(dt)
             fe.updated(dt)
 
-        # 本文の抜粋
-        body = post.get('body', {})
+        # 本文の抜粋（ログインなしのため、有料投稿のbodyはNoneになります）
+        body = post.get('body')
+        fee = post.get('feeRequired', 0)
         content = ""
-        if 'text' in body and body['text']:
-            clean_text = re.sub(r'<.*?>', '', body['text'])
-            content = clean_text[:150] + '...'
-        elif 'images' in body and body['images']:
-            content = f"画像が {len(body['images'])} 枚投稿されました"
-        elif 'files' in body and body['files']:
-            content = "ファイルが投稿されました"
+
+        if body:
+            # 無料公開されている場合の処理
+            if 'text' in body and body['text']:
+                clean_text = re.sub(r'<.*?>', '', body['text'])
+                content = clean_text[:150] + '...'
+            elif 'images' in body and body['images']:
+                content = f"画像が {len(body['images'])} 枚公開されています"
+            elif 'files' in body and body['files']:
+                content = "ファイルが公開されています"
+        else:
+            # 本文がない ＝ 有料限定投稿の場合の処理
+            if fee > 0:
+                content = f"【{fee}円プラン以上の限定投稿です。本文はFANBOXで確認してください】"
+            else:
+                content = "本文はありません"
 
         fe.content(content, type='text')
 
